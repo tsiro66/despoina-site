77f7fc81cd44d51f0eb521218e5b7f81e4418477
Thodoris Tsironis <theogr99@gmail.com>
2026-04-17 19:38:10 +0300

    Added test fonts and colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [ tailwindcss()]
-    }
+    },
+    fonts: [
+        {
+            name: "Syne",
+            cssVariable: "--font-syne",
+            provider: fontProviders.google(),
+        },
+        {
+            name: "Geom",
+            cssVariable: "--font-geom",
+            provider: fontProviders.google(),
+        },
+        {
+            name: "Google Sans",
+            cssVariable: "--font-google-sans",
+            provider: fontProviders.google(),
+        },
+    ],
 });
