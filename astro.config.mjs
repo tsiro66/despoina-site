601e43cb5f1703ba9a3d8f3d69651edc7917865c
Thodoris Tsironis <theogr99@gmail.com>
2026-05-04 03:25:39 +0300

    switched output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: 'server',
     adapter: node({ mode: 'standalone' }),
     integrations: [react(), keystatic()],
     vite: {
